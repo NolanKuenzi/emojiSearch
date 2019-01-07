cb6b38ccd0b08c82eaa8923dee832cad9b32b789
Nolan Kuenzi <mr.nolank@gmail.com>
2019-01-06 23:57:40 -0800

    docs update

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,11 +2,19 @@
 import React from 'react';
 import Enzyme, { shallow, mount} from 'enzyme';
 import App from '../src/app';
+import Header from '../src/Header';
+import emojisList from '../src/emojisList';
+
+jest.mock('../src/emojisList', ()=>({
+  settings: 'someSetting',
+}), { virtual: true })
+
+console.log(emojisList);
 
 describe('<App /> component', () => {
   it('Renders a <div />', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.exists()).toBe(true);
   });
-  
+//  it('Renders the <Header /> Component', () => {});
 });
