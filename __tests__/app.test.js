7ed340bb93d6d391404c7fe079908323ac494f18
Nolan Kuenzi <mr.nolank@gmail.com>
2019-01-06 22:37:12 -0800

    docs added

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import React from 'react';
-import Enzyme, { shallow, mount } from 'enzyme';
-import { App } from '../src/app.js';
+import Enzyme, { shallow, mount} from 'enzyme';
+import App from '../src/app';
 
 describe('<App /> component', () => {
   it('Renders a <div />', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.exists()).toBe(true);
   });
+  
 });
